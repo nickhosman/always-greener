40008f7e345038c9316f56e6c88d7fe28bf0a503
Nick <nickhosman@gmail.com>
2024-08-07 13:05:42 -0700

    mobile nav

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,7 +1,8 @@
 function Nav() {
     return (
-        <nav className="flex w-full gap-4 items-center justify-between bg-green-700 text-white h-12 box-border p-1">
+        <nav className="flex w-full gap-4 items-center justify-center bg-green-700 text-white h-12 box-border p-1">
             <img className="max-h-full" src="public/Always Greener - High Res(1).png" alt="" />
+            <img className="max-height-full w-3/4" src="public/flat_text.png" alt="" />
         </nav>
     );
 }
