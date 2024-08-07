3ede9bc672956ef49ea9ee5bc74c87cbd4c2492f
Nick <nickhosman@gmail.com>
2024-08-07 12:52:55 -0700

    added images

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,13 +1,7 @@
 function Nav() {
     return (
-        <nav className="flex w-full gap-4 items-center justify-between bg-green-700 text-white">
-            <div className="flex">
-                <img src="" alt="" />
-                <div className="flex flex-col">
-                    <span>Always Greener</span>
-                    <span>Lawn Care</span>
-                </div>
-            </div>
+        <nav className="flex w-full gap-4 items-center justify-between bg-green-700 text-white h-12 box-border p-1">
+            <img className="max-h-full" src="public/Always Greener - High Res(1).png" alt="" />
         </nav>
     );
 }
