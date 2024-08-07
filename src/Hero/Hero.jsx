f9afc8bcc70e5723e0de2adce96a5dfc9b0b9584
Nick <nickhosman@gmail.com>
2024-08-07 15:36:29 -0700

    navbar responsive

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -2,9 +2,11 @@ function Hero() {
     return (
         <div className="relative text-white flex h-fit overflow-hidden">
             <div className="flex flex-col gap-4 relative box-border p-4">
-                <h2 className="text-4xl w-80 font-bold">Professional Lawn Care Services</h2>
-                <p>Exclusively using Greenworks Battery Powered Mowers/Edgers/Trimmers and Blowers.</p>
-                <p>The quiet and clean way to make your yard beautiful!</p>
+                <h2 className="text-4xl w-full font-bold">Professional Lawn Care Services</h2>
+                <div className="flex flex-col gap-4 bg-slate-800 bg-opacity-40">
+                    <p>Exclusively using Greenworks Battery Powered Mowers/Edgers/Trimmers and Blowers.</p>
+                    <p>The quiet and <span className="text-lime-200">clean</span> way to make your yard beautiful!</p>
+                </div>
                 <button className="bg-green-700 text-white p-4 rounded-lg font-bold cursor-pointer w-40 self-center">GET IN TOUCH</button>
             </div>
             <img className="absolute -z-10 object-cover" src="https://images.unsplash.com/photo-1710580889851-b85789acd18c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGxhd258ZW58MHx8MHx8fDA%3D" alt="" />
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,8 +1,8 @@
 function Nav() {
     return (
-        <nav className="flex w-full gap-4 items-center justify-center bg-green-700 text-white h-12 box-border p-1">
+        <nav className="flex w-full gap-4 items-center justify-center md:justify-start bg-green-700 text-white h-12 box-border p-1">
             <img className="max-h-full" src="public/Always Greener - High Res(1).png" alt="" />
-            <img className="max-height-full w-3/4" src="public/flat_text.png" alt="" />
+            <img className="max-h-full max-w-3/4" src="public/flat_text.png" alt="" />
         </nav>
     );
 }
