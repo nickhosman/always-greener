dfb250a4b3c41da3c869d41b5c58288176fc12b0
Nick <nickhosman@gmail.com>
2024-08-07 22:12:29 -0700

    first draft complete

---
+++ src/Featured/Compare.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function Compare ({ img1, img2 }) {
+    const [opacity, setOpacity] = useState(0);
+
+    return (
+        <div className="flex flex-col gap-4 items-center">
+            <input
+                type="range"
+                min={0}
+                max={100}
+                value={opacity}
+                onChange={(e) => setOpacity(e.target.value)} />
+            <div className="relative">
+                <img className="absolute top-0 z-10 rounded-md" style={{ opacity: `${opacity < 10 ? `.0${opacity}` : opacity < 100 ? `.${opacity}` : 1}`}} src={img1} alt="" />
+                <img className="rounded-md" src={img2} alt="" />
+            </div>
+        </div>
+    );
+};
--- src/Featured/Featured.jsx
+++ src/Featured/Featured.jsx
@@ -1,12 +1,58 @@
+import { useState } from "react";
+import Compare from "./Compare"
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+
 export default function Featured () {
+    const imageSet = {
+        1: {
+            img1: "/1 green spot.jpeg",
+            img2: "/1 brown spot.jpeg"
+        },
+        2: {
+            img1: "/7 green spots.jpeg",
+            img2: "/7 brown spots.jpeg"
+        },
+        3: {
+            img1: "/Green Patch.jpeg",
+            img2: "/brown patch.jpeg"
+        },
+    };
+
+    const [ count, setCount ] = useState(1);
+
+    const handleLeft = () => {
+        if (count > 1) {
+            setCount(count - 1);
+        } else {
+            setCount(3);
+        }
+    };
+
+    const handleRight = () => {
+        if (count < 3) {
+            setCount(count + 1);
+        } else {
+            setCount(1);
+        }
+    };
+
     return (
-        <div className="bg-green-700">
-            <h2 className="text-white text-4xl font-bold p-4">Brown Spot Renewal</h2>
-            <div className="flex flex-col gap-4 p-4">
-                <div>
-                    
+        <div className="box-border p-4 bg-green-700 text-white">
+            <div className="flex flex-col gap-4">
+                <h2 className="text-4xl font-bold text-center">Brown Spot Renewal</h2>
+                <p className="text-center lg:text-lg">We use the same products that the best golf courses use to keep their greens looking perfect!</p>
+            </div>
+            <div className="grid sm:grid-cols-2 justify-between items-center box-border p-4 gap-8 md:flex-row px-10">
+                <div className="flex flex-col gap-4 items-center h-full w-full">
+                    <img className="rounded-md drop-shadow-md h-full object-cover" src="https://m.media-amazon.com/images/I/61MQy1pH1OL._AC_.jpg" alt="endurant premium grass paint" />
                 </div>
-                <p className="text-white">We use the same technology that the best golf courses use to keep their greens looking perfect.</p>
+                <div className="flex flex-col gap-4 border-box h-full w-full">
+                    <div className="flex relative flex-grow-0">
+                        <FaArrowCircleLeft className="absolute text-4xl lg:text-6xl cursor-pointer left-4 inset-1/2 z-20" onClick={handleLeft} />
+                        <Compare img1={imageSet[count].img1} img2={imageSet[count].img2} />
+                        <FaArrowCircleRight className="absolute text-4xl lg:text-6xl cursor-pointer right-4 top-1/2 z-20" onClick={handleRight} />
+                    </div>
+                </div>
             </div>
         </div>
     )
