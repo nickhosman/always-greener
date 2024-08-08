f6569df0875ab0bfe7d38943e33e9872a496f0a6
Nick <nickhosman@gmail.com>
2024-08-07 22:43:38 -0700

    reorganizing

--- src/Featured/Compare.jsx
+++ src/Featured/Compare.jsx
@@ -5,16 +5,16 @@ export default function Compare ({ img1, img2 }) {
 
     return (
         <div className="flex flex-col gap-4 items-center">
+            <div className="relative">
+                <img className="absolute top-0 z-10 rounded-md" style={{ opacity: `${opacity < 10 ? `.0${opacity}` : opacity < 100 ? `.${opacity}` : 1}`}} src={img1} alt="" />
+                <img className="rounded-md" src={img2} alt="" />
+            </div>
             <input
                 type="range"
                 min={0}
                 max={100}
                 value={opacity}
                 onChange={(e) => setOpacity(e.target.value)} />
-            <div className="relative">
-                <img className="absolute top-0 z-10 rounded-md" style={{ opacity: `${opacity < 10 ? `.0${opacity}` : opacity < 100 ? `.${opacity}` : 1}`}} src={img1} alt="" />
-                <img className="rounded-md" src={img2} alt="" />
-            </div>
         </div>
     );
 };
